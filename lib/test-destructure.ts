1572ad02df6fb73bce8525eb9f7d0134ce13ba95
Mohamed Awadalla <mohamed.awadalla@gmail.com>
2026-05-13 05:11:42 +0300

    fix: remove test-destructure.ts file causing build errors

--- lib/test-destructure.ts
+++
@@ -1,9 +0,0 @@
-// test-destructure.ts (temporary)
-async function test() {
-  // Simulate Supabase response
-  const response = {  { user: { id: '123' } }, error: null };
-  
-  // This should work:
-  const {  { user } } = response;
-  console.log(user?.id); // Should print: '123'
-}
